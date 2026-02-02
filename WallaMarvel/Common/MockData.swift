8ebf0c81386f68c53d1f6d73c3b3baf538de3ebe
Fatima <fatima.syed@inqbarna.com>
2026-02-02 22:33:56 +0100

    fixed urls to avoid console task domain unsupported url error

--- WallaMarvel/Common/MockData.swift
+++ WallaMarvel/Common/MockData.swift
@@ -4,12 +4,12 @@ enum MockData {
     static let charactersDataModel = CharactersDataModel(
         info: PaginationInfo(count: 2, totalPages: 5, previousPage: nil, nextPage: "page=2"),
         data: [
-            CharacterDataModel(id: 1, name: "Mickey Mouse", imageUrl: "url1", films: []),
-            CharacterDataModel(id: 2, name: "Elsa", imageUrl: "url2", films: []),
-            CharacterDataModel(id: 3, name: "Barbie", imageUrl: "url3", films: []),
-            CharacterDataModel(id: 4, name: "Simba", imageUrl: "url4", films: []),
-            CharacterDataModel(id: 5, name: "Ariel", imageUrl: "url5", films: []),
-            CharacterDataModel(id: 6, name: "Woody", imageUrl: "url6", films: [])
+            CharacterDataModel(id: 1, name: "Mickey Mouse", imageUrl: "https://example.com/mickey.jpg", films: []),
+            CharacterDataModel(id: 2, name: "Elsa", imageUrl: "https://example.com/elsa.jpg", films: []),
+            CharacterDataModel(id: 3, name: "Barbie", imageUrl: "https://example.com/barbie.jpg", films: []),
+            CharacterDataModel(id: 4, name: "Simba", imageUrl: "https://example.com/simba.jpg", films: []),
+            CharacterDataModel(id: 5, name: "Ariel", imageUrl: "https://example.com/ariel.jpg", films: []),
+            CharacterDataModel(id: 6, name: "Woody", imageUrl: "https://example.com/woody.jpg", films: [])
         ]
     )
 
--- WallaMarvelTests/Mocks/MockData.swift
+++ WallaMarvelTests/Mocks/MockData.swift
@@ -5,19 +5,19 @@ enum MockData {
     static let charactersDataModel = CharactersDataModel(
         info: PaginationInfo(count: 2, totalPages: 5, previousPage: nil, nextPage: "page=2"),
         data: [
-            CharacterDataModel(id: 1, name: "Mickey Mouse", imageUrl: "url1", films: []),
-            CharacterDataModel(id: 2, name: "Elsa", imageUrl: "url2", films: [])
+            CharacterDataModel(id: 1, name: "Mickey Mouse", imageUrl: "https://example.com/mickey.jpg", films: []),
+            CharacterDataModel(id: 2, name: "Elsa", imageUrl: "https://example.com/elsa.jpg", films: [])
         ]
     )
 
-    static let characterDataModel = CharacterDataModel(id: 1, name: "Mickey Mouse", imageUrl: "url", films: [])
+    static let characterDataModel = CharacterDataModel(id: 1, name: "Mickey Mouse", imageUrl: "https://example.com/mickey.jpg", films: [])
     static let characters = Characters(from: charactersDataModel, currentPage: 1)
 
     static let charactersDataModelNoNextPage = CharactersDataModel(
         info: PaginationInfo(count: 2, totalPages: 1, previousPage: nil, nextPage: nil),
         data: [
-            CharacterDataModel(id: 1, name: "Mickey Mouse", imageUrl: "url1", films: []),
-            CharacterDataModel(id: 2, name: "Elsa", imageUrl: "url2", films: [])
+            CharacterDataModel(id: 1, name: "Mickey Mouse", imageUrl: "https://example.com/mickey.jpg", films: []),
+            CharacterDataModel(id: 2, name: "Elsa", imageUrl: "https://example.com/elsa.jpg", films: [])
         ]
     )
 
@@ -33,7 +33,7 @@ enum MockData {
     static let singleCharacterDataModel = CharactersDataModel(
         info: PaginationInfo(count: 1, totalPages: 1, previousPage: nil, nextPage: nil),
         data: [
-            CharacterDataModel(id: 1, name: "Mickey Mouse", imageUrl: "url1", films: [])
+            CharacterDataModel(id: 1, name: "Mickey Mouse", imageUrl: "https://example.com/mickey.jpg", films: [])
         ]
     )
 
